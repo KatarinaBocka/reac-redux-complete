a0a543beb64c24bc4335c26dfd17b23e34a9864c
KatarinaBocka <katarina.bocka@gmail.com>
2019-01-30 10:59:51 +0100

    The React Router

---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="container">
+            <h4 className="center">Home</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, sunt? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi impedit asperiores quaerat atque repellat illum quasi error maiores tempore veritatis. Vero rem eos natus, doloremque quis dolor sit consequuntur quia.</p>
+        </div>
+    )
+}
+
+export default Home;
